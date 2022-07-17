89ad79e614c6255056822314fdbd36362b01f094
Ashish Deb <80671044+AndyyAD@users.noreply.github.com>
2022-07-17 14:40:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,58 +1,88 @@
-console.log("Welcome to Tic-Tac-Toe")
-let turn = "X"
-let isgameover = false;
+const cells = document.querySelectorAll(".box");
+const statusText = document.querySelector("#turn-info");
+const resetBtn = document.querySelector("#reset");
+const win = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let options = ["", "", "", "", "", "", "", "", ""];
+let turn = "X";
+let running = false;
 
-//Function to change the turn
-const changeTurn = ()=> {
-    return turn === "X"? "O" : "X"
+playGame();
+
+function playGame() {
+    cells.forEach(cell => cell.addEventListener("click", boxClicked));
+    resetBtn.addEventListener("click", resetGame);
+    statusText.textContent = `${turn}'s turn`;
+    running = true;
 }
 
-//Function to check win
-const checkWin = ()=> {
-    let boxtext = document.getElementsByClassName('boxtext');
-    let win = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ]
-    win.forEach(e =>{
-        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")) {
-            document.querySelector('.win-msg  ').innerText = boxtext[e[0]].innerText + " Won..!"
-            document.querySelector('.pop-win-msg').style.display = "block"
-            isgameover = true;
-        }
-    })
+function boxClicked() {
+    const cellIndex = this.getAttribute("cellIndex");
+
+    if(options[cellIndex] != "" || !running){
+        return;
+    }
+
+    updateBox(this, cellIndex);
+    checkWin();
 }
 
-// game function
-let boxes = document.getElementsByClassName("box");
-Array.from(boxes).forEach(element =>{
-    let boxtexts = element.querySelector('.boxtext');
-    element.addEventListener('click', ()=>{
-        if(boxtexts.innerText === '') {
-            boxtexts.innerText = turn;
-            turn = changeTurn();
-            checkWin();
-            if (!isgameover){
-                document.getElementsByClassName("info")[0].innerText  = turn + " Turn";
-            } 
-        }
-    })
-})
+function updateBox(cell, index) {
+    options[index] = turn;
+    cell.textContent = turn;
+}
 
-//reset function
-reset.addEventListener('click', ()=>{
-    let boxtexts = document.querySelectorAll('.boxtext');
-    Array.from(boxtexts).forEach(element =>{
-        element.innerText = " ";
-        turn = "X";
-        isgameover = false;
-        document.getElementsByClassName("info")[0].innerText  = turn + " Turn";
-        document.querySelector('.pop-win-msg').style.display = "none"
-    })
-}) 
+function changeTurn() {
+    turn = (turn == "X") ? "O" : "X";
+    statusText.textContent = `${turn}'s turn`;
+}
+
+function checkWin() {
+    let roundWon = false;
+
+    for(let i = 0; i< win.length; i++) {
+        const condition = win[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+
+        if(cellA == "" || cellB == "" || cellC == ""){
+            continue;
+        }
+        if(cellA == cellB && cellB == cellC){
+            roundWon = true;
+            break;
+        }
+    }
+
+    if(roundWon){
+        document.querySelector(".win-msg").innerText = `${turn}'s Won..!`
+        document.querySelector(".pop-win-msg").style.display = "block"
+        running = false;
+    }
+    else if(!options.includes("")){
+        document.querySelector(".win-msg").innerText = `It's Draw..!`
+        document.querySelector(".pop-win-msg").style.display = "block"
+        running = false;
+    }
+    else{
+        changeTurn();
+    }
+}
+
+function resetGame() {
+    turn = "X";
+    options = ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent = `${turn}'s turn`;
+    cells.forEach(cell => cell.textContent = "");
+    running=true;
+    document.querySelector(".pop-win-msg").style.display = "none"
+}
